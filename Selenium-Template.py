1caba7dd2004da90633fe6110bd6a00b8baf0065
wedcf120 <wedcf120@gmail.com>
2023-04-30 09:48:04 +0800

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -20,7 +20,7 @@ options.add_argument('--disable-gpu')
 options.add_argument('--hide-scrollbars')
 options.add_argument("--headless")
 
-html_list = []
+
 
 with open('users.txt', 'r') as f:
     for user in f:
@@ -28,10 +28,9 @@ with open('users.txt', 'r') as f:
         url = f'https://www.tiktok.com/@{user}'
         response = requests.get(url)
         if response.status_code == 2000:
-            if 'videoQuality' not in response.text:
+            if '' not in response.text:
                 print(f"An error occurred while scraping user {user}: page source does not contain 'videoQuality'")
                 continue
-            html_list.append(response.text)
         else:
             try:
                 os.system('killall -9 chrome')
@@ -40,87 +39,83 @@ with open('users.txt', 'r') as f:
                 driver.get(url)
                 time.sleep(5)
                 page_source = driver.page_source
-                if 'videoQuality' not in page_source:
+                if '' not in page_source:
                     print(f"An error occurred while scraping user {user}: page source does not contain 'videoQuality'")
                     continue
-                html_list.append(page_source)
+                html=page_source
                 driver.quit()
             except Exception as e:
                 print(f"An exception occurred while scraping user {user}: {e}")
 
-html = '\n'.join(html_list)
+
+        # Get the current time
+        now = datetime.now()
+
+        # Only include articles from the past 100 days
+        five_days_ago = now - timedelta(days=100)
 
 
 
+        regex_link = r'scheduleTime.+?\"video\"\:\{\"id\"\:\"[0-9]{10,23}\"\,'
+        regex_tit = r'contents\"\:\[\{\"desc\"\:\".*?\",'
+        regex_con = r'originCover\"\:\"(.+?)\"'
+        regex_pubdate = r'\"createTime\"\:\"[0-9]{7,20}\"\,\"scheduleTime'
+        regex_author = r'\}\,\"author\"\:\"(.+?)\"'
 
-
-# Get the current time
-now = datetime.now()
-
-# Only include articles from the past 5 days
-five_days_ago = now - timedelta(days=5)
-
-
-
-regex_link = r'scheduleTime.+?\"video\"\:\{\"id\"\:\"[0-9]{10,23}\"\,'
-regex_tit = r'contents\"\:\[\{\"desc\"\:\".*?\",'
-regex_con = r'originCover\"\:\"(.+?)\"'
-regex_pubdate = r'\"createTime\"\:\"[0-9]{7,20}\"\,\"scheduleTime'
-regex_author = r'\}\,\"author\"\:\"(.+?)\"'
-
-header = '''<?xml version="1.0" encoding="utf-8"?>
-<?xml-stylesheet type="text/xsl" href="rss1.xsl"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:media="http://search.yahoo.com/mrss/">
-<channel>
- <title>tiktok</title>
- <link>http://www.tiktok.com/</link>
- <atom:link href="http://www.tiktok.com/" rel="self" type="application/rss+xml" />
-
+        header = '''<?xml version="1.0" encoding="UTF-8"?>
+<rss  xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+    <channel>
+        <title>Tiktok @''' + user + '''</title>
+        <link>https://tiktok.com</link>
+        <atom:link href="https://tiktok.com" rel="self" type="application/rss+xml" />
  '''
 
-footer = '</channel></rss>'
+        footer = '</channel></rss>'
 
-if re.findall(regex_link, html) and re.findall(regex_tit, html):
-    links = re.findall(regex_link, html)
-    titles = re.findall(regex_tit, html)
-    articles = re.findall(regex_con, html)
-    pubdates = re.findall(regex_pubdate, html)
-    authors = re.findall(regex_author, html)
+        if re.findall(regex_link, html) and re.findall(regex_tit, html):
+            links = re.findall(regex_link, html)
+            titles = re.findall(regex_tit, html)
+            articles = re.findall(regex_con, html)
+            pubdates = re.findall(regex_pubdate, html)
+            authors = re.findall(regex_author, html)
 
-    # 将文章按发布时间从新到旧排序
-    sorted_articles = sorted(zip(pubdates, links, titles, articles, authors), reverse=True)
+            # 将文章按发布时间从新到旧排序
+            sorted_articles = sorted(zip(pubdates, links, titles, articles, authors), reverse=True)
 
-    rss = ""
+            rss = ""
 
-    # 只保留最新的50个文章
-    for pubdate, link, title, article, author in sorted_articles[:50]:
-        pubdate = re.sub(r'.*([0-9]{10}).*', r'\1', pubdate)
-        dt = datetime.fromtimestamp(int(pubdate))
-        formatted_date = dt.strftime('%a, %d %b %Y %H:%M:%S %z')
-        author = re.sub(r'\}\,\"author\"\:\"(.+?)\"', r'\1', author)
-        link = re.sub(r'scheduleTime.+?\"video\"\:\{\"id\"\:\"([0-9]{10,23})\"\,', r'https://www.tiktok.com/@' + author + r'/video/\1', link)
-        title = re.sub(r'contents\"\:\[\{\"desc\"\:\"(.*?)\"', r'\1', title.encode('utf-8').decode('unicode_escape'))
-        article = re.sub(r'originCover\"\:\"(.+?)\"', '\1', article.encode('utf-8').decode('unicode_escape'))
-        if dt >= five_days_ago:
-          rss += f'''
+            # 只保留最新的30个文章
+            for pubdate, link, title, article, author in sorted_articles[:30]:
+                pubdate = re.sub(r'.*([0-9]{10}).*', r'\1', pubdate)
+                dt = datetime.fromtimestamp(int(pubdate))
+                formatted_date = dt.strftime('%a, %d %b %Y %H:%M:%S %z')
+                author = re.sub(r'\}\,\"author\"\:\"(.+?)\"', r'\1', author)
+                link = re.sub(r'scheduleTime.+?\"video\"\:\{\"id\"\:\"([0-9]{10,23})\"\,', r'https://www.tiktok.com/@' + author + r'/video/\1', link)
+                title = re.sub(r'contents\"\:\[\{\"desc\"\:\"(.*?)\"', r'\1', title.encode('utf-8').decode('unicode_escape'))
+                title = re.sub(r'contents\"\:\[\{\"desc\"\:\"|\,$', r'', title)
+                article = re.sub(r'originCover\"\:\"(.+?)\"', '\1', article.encode('utf-8').decode('unicode_escape'))
+                if dt >= five_days_ago:
+                  rss += f'''
                 <item>
-                <title><![CDATA[{title}]]]></title>
+                <title><![CDATA[{title}]]></title>
                 <link><![CDATA[{link}]]></link>
-                <description><![CDATA[{article}]]></description>
+                <description><![CDATA[<img src="{article}"  width="30%" />]]></description>
                 <pubDate><![CDATA[{formatted_date}]]></pubDate>
                 <author><![CDATA[{author}]]></author>
                 </item>
 
                 '''
 
-    rss_feed = header + rss + footer
+            rss_feed = header + rss + footer
 
-    print(rss_feed)
+            print(rss_feed)
 
-else:
-    rss = f'{header}\n\t<item>\n\t\t<title>出错，请检查</title>\n\t\t<link></link>\n\t</item>\n{footer}'
-    print(rss)
+        else:
+            now = datetime.now()
+            formatted_date = now.strftime('%Y-%m-%d %H')
+            rss_feed = f'{header}\n\t<item>\n\t\t<title>{user}出错，请检查 {formatted_date}</title>\n\t\t<link>https://www.tiktok.com/@{user}#{formatted_date}</link>\n\t</item>\n{footer}'
+            print(rss_feed)
     
-with open('./tiktok.xml', 'w', encoding='utf-8') as f:
-    f.write(rss_feed)
+        with open('./' + user + '-tiktok.xml', 'w', encoding='utf-8') as f:
+            f.write(rss_feed)
 
